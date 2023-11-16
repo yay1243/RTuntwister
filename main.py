b61775de98a0695361f48dfd7be07c4e37d26321
yay1243 <chinhanwen@hotmail.com>
2023-11-17 00:47:32 +0800

    Shameless

--- main.py
+++ main.py
@@ -1,34 +1,29 @@
 import random
+import MT19937
+import logging
 
 
-def untwist(output):
+def unscramble(output):
     xor_last = output >> 18
     output_third = output ^ xor_last
     xor_third = (output_third << 15 & 0xefc60000)
     output_second = output_third ^ xor_third
-    a = output_second
-    b = 0x9d2c5680 >> 7
-    c = (a & b) << 7
-    d = output_second ^ c
-    e = d
-    f = 0x9d2c5680 >> 7
-    g = (e & f) << 7
-    h = output_second ^ g
-    i = h
-    j = 0x9d2c5680 >> 7
-    k = (i & j) << 7
-    l = output_second ^ k
+    aligned_const = 0x9d2c5680 >> 7
+    intermediate = 0
+    for count in range(3):
+        a = output_second ^ intermediate
+        intermediate = (a & aligned_const) << 7
+    l = output_second ^ intermediate
     xor_second = ((l << 7) & 0x9d2c5680)
     output_first = output_second ^ xor_second
     m = output_first >> 11
     n = output_first ^ m
     o = n >> 11
     initial = output_first ^ o
-    # print("Untwisted value: " + format(initial, 'b') + ", " + str(initial))
     return initial
 
 
-def twist(input):
+def scramble(input):
     # all funcs work as expected
     twisted = input ^ (input >> 11)
     twisted = twisted ^ ((twisted << 7) & 0x9d2c5680)
@@ -38,20 +33,30 @@ def twist(input):
 
 
 if __name__ == '__main__':
-    random.seed(1)
-    #1100/0011/1000/0110/1011/1011/1100/0100
-    # 3280387012
-    #print(format(random.randint(0, (2**32)-1), 'b'))
-    a = random.randint(0, (2**32)-2)
-    print(a)
-    print(twist(a))
-    untwist(twist(a))
-    for b in range(2**22):
-        c = random.randint(0, 40)
-        assert c == untwist(twist(c))
-        if b % 100000 == 0:
-            print(b)
-    #a = random.randbytes(4)
-    #print(format(random.getrandbits(8), "b"))
+    MT19937.mt_seed(1)
+    state_values = []
+    output_values = []
+    used_state_value = []
+    for a in range(1100):
+        MT19937.extract_number()
+    for a in range(1000):
+        used_state_value.append(MT19937.MT[MT19937.index % MT19937.n])
+        value_out = MT19937.extract_number()
+        output_values.append(value_out)
+        unscrambled_out = unscramble(value_out)
+        state_values.append(unscrambled_out)
+    #print(MT19937.cur_state)
+    print(output_values)
+    #print(used_state_value)
+    #print(state_values)
+    MT19937.MT = state_values[:624]
+    MT19937.index = 0
+    output_values_recreated = []
+    for a in range(1000):
+        output_values_recreated.append(MT19937.extract_number())
+    print(output_values_recreated)
+    for a in range (1000000):
+        b = MT19937.extract_number()
+        assert(b == unscramble(scramble(b)))
 
 
